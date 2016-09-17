fb95803613d65815f4f43f7160415d4605c33f71
Oscar Hallström <oscar.hallstrom@accedo.tv>
2016-09-17 18:22:54 +0200

    Initial Commit

---
+++ OppetArkivSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OppetArkivSwift
+//
+//  Created by Oscar Hallström on 2016-09-17.
+//  Copyright © 2016 Oscar Hallström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
