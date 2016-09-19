329a450f056004001e74f52a62bdf384c0213d10
Oscar Hallström <oscar.hallstrom@accedo.tv>
2016-09-19 13:23:37 +0200

    update to new initial tableviewcontroller

---
+++ OppetArkivSwift/Controller/ProgramListTableViewController.swift
@@ -0,0 +1,147 @@
+//
+//  ProgramListTableViewController.swift
+//  OppetArkivSwift
+//
+//  Created by Oscar Hallström on 2016-09-19.
+//  Copyright © 2016 Oscar Hallström. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import HTMLReader
+
+class ProgramListTableViewController: UITableViewController {
+    var programsList : [HTMLElement] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        buildAllProgramsList("http://www.oppetarkiv.se/program")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return programsList.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ProgramListCell", forIndexPath: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = programsList[indexPath.row].textContent
+        
+        return cell
+    }
+ 
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    func updateProgramsList(newList : [HTMLElement]) {
+        self.programsList = newList
+        for element in newList {
+            //            print(element)
+        }
+        self.tableView.reloadData()
+        
+    }
+    
+    func buildAllProgramsList(url: String){
+        
+        
+        //       "http://www.oppetarkiv.se/program"
+        
+        Alamofire.request(.GET, url)
+            .responseString { responseString in
+                guard responseString.result.error == nil else {
+                    // completionHandler(responseString.result.error!)
+                    return
+                    
+                }
+                guard let htmlAsString = responseString.result.value else {
+                    let error = Error.errorWithCode(.StringSerializationFailed, failureReason: "Could not get HTML as String")
+                    // completionHandler(error)
+                    
+                    return
+                }
+                //                print(htmlAsString)
+                let doc = HTMLDocument(string: htmlAsString)
+                
+                //                // find the table of charts in the HTML
+                let tables = doc.nodesMatchingSelector(".svtoa-anchor-list-link")
+                
+                var chartsTable:HTMLElement?
+                for table in tables {
+                    //                    if let tableElement = table as? HTMLElement {
+                    //                        if self.isChartsTable(tableElement) {
+                    //                            chartsTable = tableElement
+                    //                            break
+                    //                        }
+                    //print(table.textContent)
+                }
+                self.updateProgramsList(tables)
+                
+        }
+        
+    }
+}
